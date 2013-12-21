3028983369a662a149bc8bc04f9fcba705b6d45b
numa08 <n511287@gmail.com>
2013-12-21 16:46:15 +0900

    バージョンを1.0.0としました

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'n511287@ggmail.com'
 license          'All rights reserved'
 description      'Installs/Configures numa08_net'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
