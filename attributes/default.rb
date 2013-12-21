3619024d898159ddd355b529633635a51b6b7146
numa08 <n511287@gmail.com>
2013-12-21 14:06:31 +0900

    アプリケーションを配置するレシピを記述しました

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default["numa08"]["home"] = "/usr/local/etc/numa08"
+default["numa08"]["app_dir"] = "numa08_net-2.0"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+cookbook_file "/usr/local/etc/numa08_net.zip" do
+	mode 0644
+end
+
+bash "install numa08" do
+	code <<-EOC
+		rm -rf #{node.numa08.home}
+		cd /usr/local/etc
+		unzip -o numa08_net.zip
+		mv #{node.numa08.app_dir} #{node.numa08.home}
+	EOC
+end
