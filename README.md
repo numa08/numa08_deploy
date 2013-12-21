6bc0add6168600de3bc9e6ebf4cb6330a3119c72
numa08 <n511287@gmail.com>
2013-12-21 13:13:09 +0900

    最初のコミット

---
+++ README.md
@@ -0,0 +1,4 @@
+numa08_net Cookbook
+===================
+
+[numa08.net](http://numa08.net)をデプロイするためのレシピ
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'numa08_net'
+maintainer       'numa08.net'
+maintainer_email 'n511287@ggmail.com'
+license          'All rights reserved'
+description      'Installs/Configures numa08_net'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
