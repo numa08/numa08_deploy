fe307e3ef0fa61dabccb4355395cd2a7d189ce28
numa08 <n511287@gmail.com>
2013-12-23 16:00:08 +0900

    アプリケーションのバージョンと実行ポートを変更しました

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default["numa08"]["version"] = "1.0.0"
+default["numa08"]["version"] = "1.0.1"
 default["numa08"]["home"] = "/usr/local/etc/numa08"
-default["numa08"]["app_dir"] = "numa08_net-2.0"
-default["numa08"]["run_port"] = "9000"
+default["numa08"]["app_dir"] = "numa08_net-2.1"
+default["numa08"]["run_port"] = "9100"
